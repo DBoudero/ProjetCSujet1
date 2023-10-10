eda9254571d0cb7ed4f8653198e98146a0c822e4
etud user <etud@port-hp-probook650G8-1>
2023-10-10 09:18:20 +0200

    add menu v1

--- projet.c
+++ projet.c
@@ -3,9 +3,12 @@
 #include <string.h>
 
 // Vérification que le char n'a que des chiffres
-int verifChiffres(char str[]) {
-    for (int i = 0; i < strlen(str); i++) {
-        if (str[i] < '0' || str[i] > '9') {
+int verifChiffres(char str[])
+{
+    for (int i = 0; i < strlen(str); i++)
+    {
+        if (str[i] < '0' || str[i] > '9')
+        {
             return 1;
         }
     }
@@ -14,11 +17,14 @@ int verifChiffres(char str[]) {
 
 // Convertisseur binaire to decimal
 
-int binaryToDecimal(const char binaryString[]) {
+int binaryToDecimal(const char binaryString[])
+{
     int decimalValue = 0;
     int i = 0;
-    while (binaryString[i] != '\0') {
-        if (binaryString[i] != '0' && binaryString[i] != '1') {
+    while (binaryString[i] != '\0')
+    {
+        if (binaryString[i] != '0' && binaryString[i] != '1')
+        {
             printf("Entrée invalide. Veuillez entrer un nombre binaire valide.\n");
             return -1; // Sortie en cas d'erreur
         }
@@ -29,10 +35,12 @@ int binaryToDecimal(const char binaryString[]) {
 }
 
 // Convertisseur decimal to binaire
-void convertToBinary(char str[]) {
+void convertToBinary(char str[])
+{
     long long decimal = atoll(str);
 
-    if (decimal == 0) {
+    if (decimal == 0)
+    {
         printf("En binaire : 0\n");
         return;
     }
@@ -40,37 +48,53 @@ void convertToBinary(char str[]) {
     int binary[64];
     int i = 0;
 
-    while (decimal > 0) {
+    while (decimal > 0)
+    {
         binary[i] = decimal % 2;
         decimal = decimal / 2;
         i++;
     }
 
     printf("En binaire : ");
-    for (int j = i - 1; j >= 0; j--) {
+    for (int j = i - 1; j >= 0; j--)
+    {
         printf("%d", binary[j]);
     }
     printf("\n");
 }
 
-int main() {
-    char decimal_str[20];
-    printf("Entrez une valeur décimale : ");
-    scanf("%s", decimal_str);
-    if (verifChiffres(decimal_str) == 1) {
-        printf("L'entrée n'est pas un nombre décimal valide.\n");
-        return 1; // Quitter avec une erreur
-    }
-    convertToBinary(decimal_str);
+int main()
+{
+    int choix = 0;
+    printf("|---------------------------------|\n");
+    printf("|1 - Convertir Decimal to Binaire |\n");
+    printf("|2 - Convertir Binaire to Decimal |\n");
+    printf("|---------------------------------|\n");
+    scanf("%d", &choix);
 
-    char binaire_str[20];
-    printf("Entrez une valeur binaire : ");
-    scanf("%s", binaire_str);
-    if (verifChiffres(binaire_str) == 1) {
-        printf("L'entrée n'est pas un nombre binaire valide.\n");
-        return 1; // Quitter avec une erreur
+    if (choix == 1)
+    {
+        char decimal_str[20];
+        printf("Entrez une valeur décimale : ");
+        scanf("%s", decimal_str);
+        if (verifChiffres(decimal_str) == 1)
+        {
+            printf("L'entrée n'est pas un nombre décimal valide.\n");
+            return 1; // Quitter avec une erreur
+        }
+        convertToBinary(decimal_str);
     }
-    printf("%d\n",binaryToDecimal(binaire_str));
-
+    else if (choix == 2)
+    {
+        char binaire_str[20];
+        printf("Entrez une valeur binaire : ");
+        scanf("%s", binaire_str);
+        if (verifChiffres(binaire_str) == 1)
+        {
+            printf("L'entrée n'est pas un nombre binaire valide.\n");
+            return 1; // Quitter avec une erreur
+        }
+        printf("%d\n", binaryToDecimal(binaire_str));
+    }
     return 0;
 }
