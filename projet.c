9aeea76e05d16fe7e0f05c50b121dec9100960c4
DBoudero <damien.boudero@laposte.net>
2023-10-10 14:00:18 +0200

    avancée addition

--- projet.c
+++ projet.c
@@ -16,7 +16,7 @@ int verifChiffres(char str[])
 }
 
 // Convertisseur binaire to decimal
-void binaryToDecimal(const char binaryString[], int *decimalValue)
+void binaryToDecimal(char binaryString[], int *decimalValue)
 {
     *decimalValue = 0;
     int i = 0;
@@ -78,13 +78,80 @@ void convertToBinary(char str[])
     }
 }
 
+void addition(char binaryString1[16], char binaryString2[16])
+{
+    int i = 0;
+    char result[16];
+    int retenue = 0;
+
+    while (binaryString1[i] != '\0' || binaryString2[i] != '\0')
+    {
+
+        if (retenue == 0)
+        {
+            // les deux sont à 1
+            if (binaryString1[i] == 1 && binaryString2[i] == 1)
+            {
+                result[i] = 0;
+                retenue = 1;
+            }
+            //les deux sont à 0
+            else if (binaryString1[i] == 0 && binaryString2[i] == 0)
+            {
+                result[i] = '0';
+            }
+            else // un est a 1 et l'autre a 0
+            {
+                result[i] = '1';
+            }
+
+        }
+        else
+        {
+            // les deux sont à 1
+            if (binaryString1[i] == 1 && binaryString2[i] == 1)
+            {
+                result[i] = '1';
+                retenue = 1;
+            }
+            //les deux sont à 0
+            else if (binaryString1[i] == 0 && binaryString2[i] == 0)
+            {
+                result[i] = '1';
+                retenue = 0;
+            }
+            else // un est a 1 et l'autre a 0
+            {
+                result[i] = '0';
+                retenue = 1;
+            }
+        }
+        i++;
+    }
+
+    printf("Resultat : ");
+    for (int j = 0; j <= 16; j++)
+    {
+        printf("%d", result[j]);
+    }
+    printf("\n");
+}
+
 int main()
 {
+    char str1[16] = {0, 1, 0, 1, 0, 1, 1, 1}; // 11101010 = 234
+    char str2[16] = {1, 1, 0, 0, 1, 0, 0, 1}; // 10010011 = 147
+    // Resultat 234 + 147 = 381
+
+    addition(str1, str2);
+
+    /*
     int choix = 0;
-    printf("|---------------------------------|\n");
+    printf(" _________________________________ \n");
+    printf("|                                 |\n");
     printf("|1 - Convertir Decimal to Binaire |\n");
     printf("|2 - Convertir Binaire to Decimal |\n");
-    printf("|---------------------------------|\n");
+    printf("|_________________________________|\n");
     scanf("%d", &choix);
 
     if (choix == 1)
@@ -101,7 +168,7 @@ int main()
     }
     else if (choix == 2)
     {
-        char binaire_str[20];
+        char binaire_str[16];
         printf("Entrez une valeur binaire : ");
         scanf("%s", binaire_str);
         if (verifChiffres(binaire_str) == 1)
@@ -112,5 +179,6 @@ int main()
         int decimalValue;
         binaryToDecimal(binaire_str, &decimalValue);
     }
+    */
     return 0;
 }
