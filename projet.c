fb324f1d57372afb1da757006b7c4c36cadaa92e
Starnort Florian <106724270+StarFlo974@users.noreply.github.com>
2023-10-26 12:34:37 +0200

    ajout fonction multiplier et resultat formater

--- projet.c
+++ projet.c
@@ -261,10 +261,31 @@ char *soustractionBinaire(char binary1[], char binary2[])
 }
 
 
-// Fonction qui prend en entrée deux tableaux de char et qui retourne le resultat de leur multiplication
+// Fonction pour supprimer les underscores d'une chaîne
+void supprimerUnderscores(char* chaine) {
+    int i, j;
+    for (i = 0, j = 0; chaine[i] != '\0'; i++) {
+        if (chaine[i] != '_') {
+            chaine[j++] = chaine[i];
+        }
+    }
+    chaine[j] = '\0';
+}
+
+// Fonction pour multiplier deux nombres binaires
 char* multiplierBinaire(const char* binaire1, const char* binaire2) {
-    int longueur1 = strlen(binaire1);
-    int longueur2 = strlen(binaire2);
+    // Supprimer les underscores des entrées
+    char binaire1SansUnderscores[19];
+    char binaire2SansUnderscores[19];
+    
+    strncpy(binaire1SansUnderscores, binaire1, sizeof(binaire1SansUnderscores));
+    supprimerUnderscores(binaire1SansUnderscores);
+
+    strncpy(binaire2SansUnderscores, binaire2, sizeof(binaire2SansUnderscores));
+    supprimerUnderscores(binaire2SansUnderscores);
+
+    int longueur1 = strlen(binaire1SansUnderscores); // Utiliser la version sans underscores
+    int longueur2 = strlen(binaire2SansUnderscores);
     int longueurResultat = longueur1 + longueur2;
     char* resultat = (char*)malloc(longueurResultat + 1);
     
@@ -274,10 +295,10 @@ char* multiplierBinaire(const char* binaire1, const char* binaire2) {
     resultat[longueurResultat] = '\0';
     
     for (int i = longueur1 - 1; i >= 0; i--) {
-        if (binaire1[i] == '1') {
+        if (binaire1SansUnderscores[i] == '1') {
             int retenue = 0;
             for (int j = longueur2 - 1; j >= 0; j--) {
-                int produit = (binaire2[j] - '0') * (binaire1[i] - '0') + (resultat[i + j + 1] - '0') + retenue;
+                int produit = (binaire2SansUnderscores[j] - '0') * (binaire1SansUnderscores[i] - '0') + (resultat[i + j + 1] - '0') + retenue;
                 retenue = produit / 2;
                 resultat[i + j + 1] = (produit % 2) + '0';
             }
@@ -287,6 +308,49 @@ char* multiplierBinaire(const char* binaire1, const char* binaire2) {
     
     return resultat;
 }
+
+// Fonction pour formater le résultat avec des zéros à gauche et des underscores tous les 4 bits
+char* formaterResultat(char* resultat) {
+    int debut = 0;
+    while (resultat[debut] == '0' && resultat[debut + 1] != '\0') {
+        debut++;
+    }
+
+    int resultatLength = strlen(&resultat[debut]);
+
+    // Calculer le nombre de zéros à gauche nécessaires pour avoir une longueur de 16 bits
+    int zerosToPad = 16 - resultatLength;
+
+    // Créer une nouvelle chaîne avec des zéros ajoutés à gauche
+    char resultat16Bits[resultatLength + zerosToPad + 1];  // 16 bits + caractère nul
+    memset(resultat16Bits, '0', zerosToPad);
+    strcpy(resultat16Bits + zerosToPad, &resultat[debut]);
+
+    // Calculer le nombre d'underscores nécessaires
+    int numUnderscores = (16 - 1) / 4;
+
+    // Longueur totale du résultat avec les underscores
+    int resultatLengthWithUnderscores = 16 + numUnderscores;
+
+    char resultatAvecUnderscores[resultatLengthWithUnderscores + 1];  // +1 pour le caractère nul
+    int i = 0;
+    int j = 0;
+
+    // Ajouter les underscores tous les 4 bits
+    while (i < resultatLengthWithUnderscores) {
+        if ((i % 5) == 4) {
+            resultatAvecUnderscores[i] = '_';
+        } else {
+            resultatAvecUnderscores[i] = resultat16Bits[j];
+            j++;
+        }
+        i++;
+    }
+    resultatAvecUnderscores[i] = '\0';
+
+    return strdup(resultatAvecUnderscores);
+}
+
 
 // Fonction qui prend en entrée deux tableaux de char et qui retourne le resultat de leur division
 char *binaryDivision(char dividend[], char divisor[]) {
@@ -471,23 +535,22 @@ int main()
     }
     else if (choix == 6)
     {
-        char binaire1[19];
-        char binaire2[19];
-        printf("Entrez un premier nombre binaire : ");
-        scanf("%s", binaire1);
-        printf("Entrez un deuxième nombre binaire : ");
-        scanf("%s", binaire2);
+    char binaire1[19];  // Vous pouvez ajuster la taille si nécessaire
+    char binaire2[19];
 
-    char* resultat = multiplierBinaire(format(binaire1), format(binaire2));
+    printf("Entrez le premier nombre binaire : ");
+    scanf("%s", binaire1);
 
-    // Supprimer les zéros non significatifs
-    int debut = 0;
-    while (resultat[debut] == '0' && resultat[debut + 1] != '\0') {
-        debut++;
-    }
-    printf("Le résultat de la multiplication est : %s\n", &resultat[debut]);
+    printf("Entrez le deuxième nombre binaire : ");
+    scanf("%s", binaire2);
+
+    char* resultat = multiplierBinaire(binaire1, binaire2);
+    char* resultatFormate = formaterResultat(resultat);
+
+    printf("Le résultat de la multiplication est : %s\n", resultatFormate);
 
     free(resultat);
-    }
+    free(resultatFormate);
+
     return 0;
 }
